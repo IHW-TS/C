20aadba37e237d3eeac90c4b7eda2c1cc032d487
Teoman Soykan <teoman.soykan1@gmail.com>
2021-03-25 22:10:59 +0100

    V2
    Exo 16

--- Soykan_Teoman_Exercice_16_N°20201470.c
+++ Soykan_Teoman_Exercice_16_N°20201470.c
@@ -11,10 +11,10 @@ unsigned int lengthString(char *s)
     int i;
 
     char s[] = "Programming is fun";
-
+    
     for ( i = 0; s[i] != '\0'; ++i);
     
-    printf("Length of the string: %d", i);
+    printf("longeur du mot : %d", i);
     return 0;
 }
 /*
