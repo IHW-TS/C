d136cd67164eb1009db706a1156bf5e5fde7c356
IHW-TS <78849961+IHW-TS@users.noreply.github.com>
2021-03-26 13:24:27 +0100

    Update Q1_Teoman_Soykan_20201470.c

--- Exercice 16/Q1_Teoman_Soykan_20201470.c
+++ Exercice 16/Q1_Teoman_Soykan_20201470.c
@@ -1,11 +1,22 @@
-int longeur_chaine(char s[]) {
+#include <stdio.h>
+#include <stdlib.h>
 
-   int chr = 0;
+unsigned int lengthString(char *s)
+{
+    int i;
+    for (i = 0; s[i] != '\0'; i++);
+    
+    printf("Longeur de la chaine : %d", i);
+    return 0;
+}
 
-   while (s[chr] != '\0')
-      chr++;
-
-   return chr;
+int main () {
+    
+    char x[100];
+    scanf("%s", &x);
+    lengthString(x);
+    return 0;
+    
 }
 /*
 Ici, la boucle while qui va compter le nombre de caratere. != signifie qu'il doit etre different de \0. "chr++" va compter le nombre de caractere.
