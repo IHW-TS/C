029112a6ed951fc099e323331a251bc57898704e
IHW-TS <78849961+IHW-TS@users.noreply.github.com>
2021-03-29 20:36:29 +0200

    Update Soykan_Teoman_Exercice_15_N°20201470.c

--- Soykan_Teoman_Exercice_15_N°20201470.c
+++ Soykan_Teoman_Exercice_15_N°20201470.c
@@ -77,3 +77,55 @@ int main()
     }
     return 0;
 }
+
+/*
+////////////////////////////////////////////////////////////////////
+V2
+///////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>   
+
+int sieve(int n){
+    
+    int prime[n+1];
+    int count = 0;
+	//Loading the array with numbers from 1 to n
+	for(int i = 1; i <= n; i++)
+	{
+		prime[i] = i;
+	}
+	//Start with least prime number, which is 2.
+	//No need to check for numbers greater than square root of n.
+	//They will be already marked.
+	for(int i = 2; i*i <= n; i++)
+	{
+		if(prime[i] != -1)
+		{
+			//Mark all the multiples of i as -1.
+			for(int j = 2*i; j <=n ; j += i)
+				prime[j] = -1;
+		}
+	}
+	printf("Prime numbers are: \n");
+	for(int i=2; i <= n; i++)
+	{
+		if(prime[i] != -1)
+		{
+			printf("%d ", i);
+			count++;
+		}
+	}
+	
+    printf("\nTotale de nombre premier inferieur ou egaux est de : %d",count); 
+
+	
+}
+
+int main()
+{
+	int n;
+	printf("Enter n value: ");
+	scanf("%d", &n);
+    sieve(n);
+}*/
