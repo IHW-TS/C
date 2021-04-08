ecbd15c2ce0ea2233e09c1cce8784d29bbec8b86
IHW-TS <78849961+IHW-TS@users.noreply.github.com>
2021-04-08 11:51:39 +0200

    Update Q4_Soykan_Teoman_N°20201470.c

--- Exercice 18/Q4_Soykan_Teoman_N°20201470.c
+++ Exercice 18/Q4_Soykan_Teoman_N°20201470.c
@@ -1,15 +1,19 @@
+#include <stdio.h>  
+#include <stdlib.h> 
 #include <math.h>
-#include <stdio.h>
 
+int entier_binaire(int n) {
 
-long long convert(int n) {
-
-    long long b = 0;
-    int reste, i = 1, etape = 1;
+    int b = 0;
+    int reste, i = 1/*, etape = 1*/;
 
     while (n != 0) {
         reste = n % 2;
-        printf("Etape 1 %d: %d/2, Reste = %d, Quotient = %d\n", etape++, n, reste, n / 2);
+        
+       // affiche les étapes pour parvenir au nombre binaire
+       
+       /* printf("Etape 1 %d: %d/2, Reste = %d, Quotient = %d\n", etape++, n, reste, n / 2); */
+       
         n /= 2;
         b += reste * i;
         i *= 10;
@@ -22,6 +26,6 @@ int main() {
     int n;
     printf("Entre un nombre entier : ");
     scanf("%d", &n);
-    printf("%d En nombre entier  = %lld en binaire", n, convert(n));
+    printf("%d En nombre entier = %d en binaire", n, entier_binaire(n));
     return 0;
 }
